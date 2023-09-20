d5240be4ddf599478abe476ba04a489ea36ab30b
gautchap <gautchap+github@outlook.com>
2023-09-20 17:24:28 +0200

    refactor : ready to deploy to vercel

---
+++ api/index.js
@@ -0,0 +1,5 @@
+
+
+
+
+export {default} from "../server/index.js";
--- server/index.ts
+++ server/index.ts
@@ -16,62 +16,81 @@ import compression from "compression";
 import { configDotenv } from "dotenv";
 import { renderPage } from "vite-plugin-ssr/server";
 import { root } from "./root.js";
+import mongoose from "mongoose";
+import { router } from "./router.js";
 const isProduction = process.env.NODE_ENV === "production";
 
 configDotenv();
 
-await startServer();
+const app = express();
 
-async function startServer() {
-    const app = express();
+app.use(compression());
 
-    app.use(compression());
-
-    // Vite integration
-    if (isProduction) {
-        // In production, we need to serve our static assets ourselves.
-        // (In dev, Vite's middleware serves our static assets.)
-        const sirv = (await import("sirv")).default;
-        app.use(sirv(`${root}/dist/client`));
-    } else {
-        // We instantiate Vite's development server and integrate its middleware to our server.
-        // ⚠️ We instantiate it only in development. (It isn't needed in production and it
-        // would unnecessarily bloat our production server.)
-        const vite = await import("vite");
-        const viteDevelopmentMiddleware = (
-            await vite.createServer({
-                root,
-                server: { middlewareMode: true },
-            })
-        ).middlewares;
-        app.use(viteDevelopmentMiddleware);
-    }
-
-    // ...
-    // Other middlewares (e.g. some RPC middleware such as Telefunc)
-    // ...
-
-    // Vite-plugin-ssr middleware. It should always be our last middleware (because it's a
-    // catch-all middleware superseding any middleware placed after it).
-    app.get("*", async (req, res, next) => {
-        const pageContextInit = {
-            urlOriginal: req.originalUrl,
-        };
-        const pageContext = await renderPage(pageContextInit);
-        const { httpResponse } = pageContext;
-        if (httpResponse) {
-            const { body, statusCode, headers, earlyHints } = httpResponse;
-            if (res.writeEarlyHints) res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) });
-            for (const [name, value] of headers) res.setHeader(name, value);
-            res.status(statusCode);
-            // For HTTP streams use httpResponse.pipe() instead, see https://vite-plugin-ssr.com/stream
-            res.send(body);
-        } else {
-            return next();
-        }
-    });
-
-    const port = process.env.PORT || 3000;
-    app.listen(port);
-    console.log(`Server running at http://localhost:${port}`);
+// Vite integration
+if (isProduction) {
+    // In production, we need to serve our static assets ourselves.
+    // (In dev, Vite's middleware serves our static assets.)
+    const sirv = (await import("sirv")).default;
+    app.use(sirv(`${root}/dist/client`));
+} else {
+    // We instantiate Vite's development server and integrate its middleware to our server.
+    // ⚠️ We instantiate it only in development. (It isn't needed in production and it
+    // would unnecessarily bloat our production server.)
+    const vite = await import("vite");
+    const viteDevelopmentMiddleware = (
+        await vite.createServer({
+            root,
+            server: { middlewareMode: true },
+        })
+    ).middlewares;
+    app.use(viteDevelopmentMiddleware);
 }
+
+// ...
+// Other middlewares (e.g. some RPC middleware such as Telefunc)
+// ...
+
+app.use(express.json());
+app.use("/api", router);
+
+// Vite-plugin-ssr middleware. It should always be our last middleware (because it's a
+// catch-all middleware superseding any middleware placed after it).
+app.get("*", async (req, res, next) => {
+    const pageContextInit = {
+        urlOriginal: req.originalUrl,
+        redirectTo: "",
+    };
+    const pageContext = await renderPage(pageContextInit);
+    const { redirectTo, httpResponse } = pageContext;
+    if (redirectTo) {
+        res.redirect(307, redirectTo);
+    } else if (httpResponse) {
+        const { body, statusCode, headers, earlyHints } = httpResponse;
+        if (res.writeEarlyHints) res.writeEarlyHints({ link: earlyHints.map((e) => e.earlyHintLink) });
+        res.setHeader("X-Content-Type-Options", "nosniff");
+        res.setHeader("X-Frame-Options", "DENY");
+        res.setHeader("X-XSS-Protection", "1");
+        res.setHeader(
+            "Content-Security-Policy",
+            "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self' fonts.googleapis.com; font-src 'self' fonts.gstatic.com; frame-ancestors 'self'; form-action 'self'; base-uri 'none'; object-src 'none'"
+        );
+        for (const [name, value] of headers) res.setHeader(name, value);
+        res.status(statusCode);
+        // For HTTP streams use httpResponse.pipe() instead, see https://vite-plugin-ssr.com/stream
+        res.send(body);
+    } else {
+        return next();
+    }
+});
+
+const port = process.env.VITE_APP_PORT || 3000;
+try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ Connexion MonboDB établie!");
+
+    app.listen(port, () => console.log(`✅ L'application écoute sur http://localhost:${port}`));
+} catch (error: any) {
+    console.log("❌ Impossible de démarrer l'application Node", error.message);
+}
+
+export default app;
